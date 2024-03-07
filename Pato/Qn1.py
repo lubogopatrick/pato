75e669df3b0ff0f977e0ce44e4aa3e4d831616b3
LUBOGO PATRICK <158350368+lubogopatrick@users.noreply.github.com>
2024-03-07 11:34:04 +0300

    Add files via upload

---
+++ Pato/Qn1.py
@@ -0,0 +1,14 @@
+
+
+salary = 500000
+allowance = 200000
+bankloan= 300000 
+food = 150000
+
+patricia_monthly_income= salary + allowance 
+print (patricia_monthly_income,"income")
+patricia_monthly_expenditure= bankloan + food
+print (patricia_monthly_expenditure,"expenditure")
+
+if (patricia_monthly_income, "income > expenditure")
+print (patricia_monthly_expenditure, "cutdown")
---
+++ Pato/Qn2.py
@@ -0,0 +1,20 @@
+
+
+diary_business_monthly_earning = 500000
+diary_business_monthly_rent = 55000
+produce_business_monthly_earning = 900000
+produce_business_monthly_rent = 200000
+
+solomon_dairy_business_saving = diary_business_monthly_earning - diary_business_monthly_rent
+print (f"solomon dairy business saving is {solomon_dairy_business_saving}, shillings")
+
+solomon_dairy_business_saving = produce_business_monthly_earning - produce_business_monthly_rent
+print(f"solomon dairy business saving is {solomon_dairy_business_saving}, shillings")
+
+solomon_dairy_business_saving = diary_business_monthly_earning - produce_business_monthly_rent
+print(f"solomon dairy business saving is {solomon_dairy_business_saving}, total")
+
+
+
+
+
