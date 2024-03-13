a376ec8f7f2c2fe5869bdeffd3a151c301d5a0fb
LUBOGO PATRICK <158350368+lubogopatrick@users.noreply.github.com>
2024-03-13 18:51:59 +0300

    Create Pato.js

---
+++ Pato.js
@@ -0,0 +1,11 @@
+//check if the number is positive
+
+const number = prompt("9: ");
+
+//check if number is greater than 0
+if (number> 0 ) {
+// the body of the if statement 
+console.log ("The number is positive");
+}
+
+console.log("The if statement is easy");
