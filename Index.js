1e51648098a4698d726191bae72708294f823919
LUBOGO PATRICK <158350368+lubogopatrick@users.noreply.github.com>
2024-03-14 19:39:25 +0300

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+impot ReactDom from reactdom / client;
+import '/index.CSS';
+
+import App from' ,/ App;
+  const root = ReactDom.create Root/ document.get root render react <App/>);
